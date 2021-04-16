14dfaa88b6904a9ee81a92ee20ba50e01cf23b5a
kishor <kishor.babu@c2info.com>
2021-04-16 11:24:31 +0530

    UI files

---
+++ liveorder/arunKnair2226-liveorder-1c65657b1fa0/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: 'Why Live Order?',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Start Selling',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Help',
+    url: '#',
+    cName: 'nav-links'
+  }
+]

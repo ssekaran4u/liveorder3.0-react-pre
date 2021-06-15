3f07c87a1f7b997f30120c2ac17fd8365aa2fb1d
Shreyas Balachandran <shreyas.bba@gmail.com>
2021-06-15 13:26:06 +0530

    replaced why Live QMarkImg with svg version

--- src/Components/WhyLiveSection/WhyLiveSection.js
+++ src/Components/WhyLiveSection/WhyLiveSection.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import QMarkImg from '../../Images/QMarkImg.png'
-import SellerImg from '../../Images/SellerImg.svg'
-import TrackingImg from '../../Images/TrackingImg.svg'
-import ReconciliationImg from '../../Images/ReconciliationImg.svg'
-import ManageTeamImg from '../../Images/ManageTeamImg.svg'
-import './WhyLiveSection.css'
-
+import React, { Component } from "react";
+import QMarkImg from "../../Images/QMarkImg.svg";
+import SellerImg from "../../Images/SellerImg.svg";
+import TrackingImg from "../../Images/TrackingImg.svg";
+import ReconciliationImg from "../../Images/ReconciliationImg.svg";
+import ManageTeamImg from "../../Images/ManageTeamImg.svg";
+import "./WhyLiveSection.css";
 
 class WhyLiveSection extends Component {
   render() {
@@ -13,12 +12,16 @@ class WhyLiveSection extends Component {
       <div className="WhyLiveSection d-lg-flex MainContainer py-4" id="whyLive">
         <div className="d-flex align-items-center">
           <div className="whyLiveSectionTitle mr-0 mr-lg-4 mb-4 mb-lg-0">
-            <img src={QMarkImg} alt="Why Live Order"className="mb-3" />
+            <img src={QMarkImg} alt="Why Live Order" className="mb-3" />
             <h3 className="h3 mb-3">Why Live Order ?</h3>
             <p className="body-copy mb-4">
-              B2B online ordering platform connecting Buyers & sellers which gives over all solution to “Buyers” in turn increasing the business of “Sellers.”
+              B2B online ordering platform connecting Buyers & sellers which
+              gives over all solution to “Buyers” in turn increasing the
+              business of “Sellers.”
             </p>
-            <button type="button" className="btn btn-primary demo-btn">Schedule A Demo</button>
+            <button type="button" className="btn btn-primary demo-btn">
+              Schedule A Demo
+            </button>
           </div>
         </div>
         <div className="SellerTrackingCardSection mr-0 mr-lg-4">
@@ -28,7 +31,10 @@ class WhyLiveSection extends Component {
                 <img src={SellerImg} alt="Find The Right Sellers" />
               </div>
               <h6 className="h6 CardSubtitle mb-4">Find The Right Sellers</h6>
-              <p className="body-copy">We help Buyers find the right Sellers offering the best prices, within their geography</p>
+              <p className="body-copy">
+                We help Buyers find the right Sellers offering the best prices,
+                within their geography
+              </p>
             </div>
           </div>
           <div className="card mb-4 mb-lg-0">
@@ -37,7 +43,10 @@ class WhyLiveSection extends Component {
                 <img src={TrackingImg} alt="Centralised Tracking" />
               </div>
               <h6 className="h6 CardSubtitle mb-4">Centralised Tracking</h6>
-              <p className="body-copy">We provide end-to-end tracking  starting from order placement to delivery of product</p>
+              <p className="body-copy">
+                We provide end-to-end tracking starting from order placement to
+                delivery of product
+              </p>
             </div>
           </div>
         </div>
@@ -45,10 +54,18 @@ class WhyLiveSection extends Component {
           <div className="card mb-4 GradientCard">
             <div className="card-body p-0">
               <div className="CardIconHolder ReconciliatonImg d-flex align-items-center justify-content-center mb-4">
-                <img src={ReconciliationImg} alt="Bill To Bill Reconciliation" />
+                <img
+                  src={ReconciliationImg}
+                  alt="Bill To Bill Reconciliation"
+                />
               </div>
-              <h6 className="h6 CardSubtitle mb-4">Bill To Bill Reconciliation</h6>
-              <p className="body-copy">Invoice reconciliation is important for keeping accounting records updated & avoid fraud</p>
+              <h6 className="h6 CardSubtitle mb-4">
+                Bill To Bill Reconciliation
+              </h6>
+              <p className="body-copy">
+                Invoice reconciliation is important for keeping accounting
+                records updated & avoid fraud
+              </p>
             </div>
           </div>
           <div className="card">
@@ -57,7 +74,10 @@ class WhyLiveSection extends Component {
                 <img src={ManageTeamImg} alt="Manage Your Entire Team" />
               </div>
               <h6 className="h6 CardSubtitle mb-4">Manage Your Entire Team</h6>
-              <p className="body-copy">Team management solution. Option to create teams & branches and manage user rights.</p>
+              <p className="body-copy">
+                Team management solution. Option to create teams & branches and
+                manage user rights.
+              </p>
             </div>
           </div>
         </div>

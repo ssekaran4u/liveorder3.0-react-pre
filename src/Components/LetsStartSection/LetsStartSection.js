db4f96af19b71d0bcf9090973ccf2f1ca8f99830
Shreyas Balachandran <shreyas.bba@gmail.com>
2021-06-17 22:39:02 +0530

    Lets start section css fix

--- src/Components/LetsStartSection/LetsStartSection.js
+++ src/Components/LetsStartSection/LetsStartSection.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import './LetsStartSection.css'
+import React, { Component } from "react";
+import "./LetsStartSection.css";
 
 class LetsStartSection extends Component {
   render() {
     return (
       <div className="LetsStartSection">
         <div className="LetsStartSectionCard position-relative text-center">
-          <h3 className="h3 text-white mb-4">Let’s Start Live Order</h3>
-          <p className="body-copy mb-5 text-white">Live Order & you, the dynamic duo.</p>
-          <button type="submit" className="btn demo-card-btn">Schedule A Demo</button>
+          <h3 className="h3 text-white">Let’s Start Live Order</h3>
+          <p className="body-copy mb-5 text-white">
+            Live Order & you, the dynamic duo.
+          </p>
+          <button type="submit" className="btn demo-card-btn">
+            Schedule A Demo
+          </button>
           <div className="purple-blob2"></div>
           <div className="green-circle"></div>
           <div className="orange-circle"></div>

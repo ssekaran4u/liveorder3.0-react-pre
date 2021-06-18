f89b7c13e61315fcaa28e43ebca5064dba5d1244
Shreyas Balachandran <shreyas.bba@gmail.com>
2021-06-18 22:52:31 +0530

    Disabled site loader temp, added terms route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import WhyLiveSection from "./Components/WhyLiveSection/WhyLiveSection";
@@ -6,6 +6,7 @@ import WhyLiveSection from "./Components/WhyLiveSection/WhyLiveSection";
 import PartnerSection from "./Components/PartnerSection/PartnerSection";
 import LetsStartSection from "./Components/LetsStartSection/LetsStartSection";
 import Testimonials from "./Components/Testimonials/Testimonials";
+import TermsConditions from "./Components/TermsConditionsSection/TermsConditionsSection";
 import NewVideo from "./Components/VideoSection/NewVideo";
 import "../node_modules/react-modal-video/css/modal-video.min.css";
 import Help from "./routes/Help";
@@ -13,9 +14,12 @@ import FooterPage from "./Components/Footer/Footer";
 import SiteLoader from "./Components/SiteLoader/SiteLoader";
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
-      <SiteLoader />
+      {/* <SiteLoader /> */}
       <Navbar />
       {window.location.pathname === "/" && (
         <>
@@ -27,11 +31,9 @@ function App() {
           <LetsStartSection />
         </>
       )}
-      {window.location.pathname === "/help" && (
-        <>
-          <Help />
-        </>
-      )}
+      {window.location.pathname === "/help" && <Help />}
+
+      {window.location.pathname === "/terms" && <TermsConditions />}
       <FooterPage />
     </>
   );

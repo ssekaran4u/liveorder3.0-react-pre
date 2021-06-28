9efa497d8e1230a96824758d073594b07ad2394e
Shreyas Balachandran <shreyas.bba@gmail.com>
2021-06-28 08:45:18 +0530

    LetsStartSecttion interactions

--- src/Components/LetsStartSection/LetsStartSection.js
+++ src/Components/LetsStartSection/LetsStartSection.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 import "./LetsStartSection.css";
+import letsStartBGSVG from "../../Images/lets-start-bg.svg";
 
 class LetsStartSection extends Component {
+  componentDidMount() {
+    let div = document.querySelector(".demo-card-btn");
+    const options = {
+      threshold: 1,
+      rootMargin: "-200px  0px 0px 0px",
+    };
+    let observer = new IntersectionObserver(function (entries, observer) {
+      entries.forEach((entry) => console.log(entry));
+      let parentDiv = document.querySelector(".LetsStartSection");
+      let onScrollTimeout;
+      if (entries[0].isIntersecting) {
+        // onScrollTimeout = setTimeout(() => {
+        parentDiv.classList.add("on_scroll");
+        // }, 750);
+        // options.rootMargin = "-100px 0px 0px 0px";
+        // } else if (!entries[0].isIntersecting) {
+        // parentDiv.classList.remove("on_scroll");
+        // options.rootMargin = "-100px 0px 0px 0px";
+        // clearTimeout(onScrollTimeout);
+      }
+    }, options);
+
+    // console.log(observer);
+    observer.observe(div);
+  }
   render() {
     return (
       <div className="LetsStartSection">
         <div className="LetsStartSectionCard position-relative text-center">
+          <img src={letsStartBGSVG} alt="Kiwi standing on oval" />
           <h3 className="h3 text-white">Let’s begin the journey!</h3>
           <p className="body-copy mb-5 text-white">
             Live Order & you, the dynamic duo.

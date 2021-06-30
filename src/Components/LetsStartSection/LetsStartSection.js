cf1c968bb4d3f1cf8fbc7add90ff349c407ab994
Shreyas Balachandran <shreyas.bba@gmail.com>
2021-06-30 11:40:22 +0530

    lets start section removed onScroll event

--- src/Components/LetsStartSection/LetsStartSection.js
+++ src/Components/LetsStartSection/LetsStartSection.js
@@ -4,29 +4,28 @@ import letsStartBGSVG from "../../Images/lets-start-bg.svg";
 
 class LetsStartSection extends Component {
   componentDidMount() {
-    let div = document.querySelector(".demo-card-btn");
-    const options = {
-      threshold: 1,
-      rootMargin: "-200px  0px 0px 0px",
-    };
-    let observer = new IntersectionObserver(function (entries, observer) {
-      entries.forEach((entry) => console.log(entry));
-      let parentDiv = document.querySelector(".LetsStartSection");
-      let onScrollTimeout;
-      if (entries[0].isIntersecting) {
-        // onScrollTimeout = setTimeout(() => {
-        parentDiv.classList.add("on_scroll");
-        // }, 750);
-        // options.rootMargin = "-100px 0px 0px 0px";
-        // } else if (!entries[0].isIntersecting) {
-        // parentDiv.classList.remove("on_scroll");
-        // options.rootMargin = "-100px 0px 0px 0px";
-        // clearTimeout(onScrollTimeout);
-      }
-    }, options);
-
-    // console.log(observer);
-    observer.observe(div);
+    // let div = document.querySelector(".demo-card-btn");
+    // const options = {
+    //   threshold: 1,
+    //   rootMargin: "-200px  0px 0px 0px",
+    // };
+    // let observer = new IntersectionObserver(function (entries, observer) {
+    //   entries.forEach((entry) => console.log(entry));
+    //   let parentDiv = document.querySelector(".LetsStartSection");
+    //   let onScrollTimeout;
+    //   if (entries[0].isIntersecting) {
+    //     // onScrollTimeout = setTimeout(() => {
+    //     parentDiv.classList.add("on_scroll");
+    //     // }, 750);
+    //     // options.rootMargin = "-100px 0px 0px 0px";
+    //     // } else if (!entries[0].isIntersecting) {
+    //     // parentDiv.classList.remove("on_scroll");
+    //     // options.rootMargin = "-100px 0px 0px 0px";
+    //     // clearTimeout(onScrollTimeout);
+    //   }
+    // }, options);
+    // // console.log(observer);
+    // observer.observe(div);
   }
   render() {
     return (
